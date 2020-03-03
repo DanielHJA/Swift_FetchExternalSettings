d47611743e98447d371b5074925c54a4397da2db
Daniel <daniel.hjartstrom@gmail.com>
2020-03-03 10:57:55 +0100

    Initial Commit

---
+++ Mapss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mapss
+//
+//  Created by Daniel Hjärtström on 2020-03-03.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
